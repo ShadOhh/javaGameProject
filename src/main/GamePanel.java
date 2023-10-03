ed184b62d3d741036d491c1aca6748d3a15f91d2
jacklian <jacklian@iu.edu>
2023-10-02 22:46:24 -0400

    Proj

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,28 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    //SCREEN SETTINGS
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+
+    final int maxScreenCol = 16;
+    final int maxScreeRow = 12;
+    final int screenWidth = tileSize*maxScreenCol;
+    final int screenHeight = tileSize*maxScreeRow;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+
+
+
+
 }
